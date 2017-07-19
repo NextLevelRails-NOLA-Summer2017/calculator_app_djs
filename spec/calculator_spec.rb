840de3ae87312177139b76888eaaf4b849cc04c9
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2017-07-19 00:39:13 -0500

    Create test for Calculator#subtract.
    Implement Calculator#subtract.

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Calculator do
       expect(Calculator.new.add(1, 2)).to eq(3)
     end
   end
+
+  describe '#subtract' do
+    it 'returns the difference of its arguments' do
+      expect(Calculator.new.subtract(3, 2)).to eq(1)
+    end
+  end
 end
